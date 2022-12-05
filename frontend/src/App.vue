82199b6ef7f7bb887519b4fd7215f749a3853d94
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-12-05 15:25:27 +0700

    ep10_seperate_menu

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,22 +1,6 @@
 <template>
   <a-layout class="h-screen">
-    <a-layout-sider v-model:collapsed="collapsed" :trigger="null" collapsible>
-      <div class="logo" />
-      <a-menu v-model:selectedKeys="selectedKeys" theme="dark" mode="inline">
-        <a-menu-item key="1">
-          <user-outlined />
-          <span>nav 1</span>
-        </a-menu-item>
-        <a-menu-item key="2">
-          <video-camera-outlined />
-          <span>nav 2</span>
-        </a-menu-item>
-        <a-menu-item key="3">
-          <upload-outlined />
-          <span>nav 3</span>
-        </a-menu-item>
-      </a-menu>
-    </a-layout-sider>
+    <Menu v-model:collapsed="collapsed" />
     <a-layout>
       <a-layout-header style="background: #fff; padding: 0">
         <menu-unfold-outlined
@@ -44,19 +28,12 @@
   </a-layout>
 </template>
 <script lang="ts">
-import {
-  UserOutlined,
-  VideoCameraOutlined,
-  UploadOutlined,
-  MenuUnfoldOutlined,
-  MenuFoldOutlined,
-} from "@ant-design/icons-vue";
+import { MenuUnfoldOutlined, MenuFoldOutlined } from "@ant-design/icons-vue";
+import Menu from "./components/core/Menu.vue";
 import { defineComponent, ref } from "vue";
 export default defineComponent({
   components: {
-    UserOutlined,
-    VideoCameraOutlined,
-    UploadOutlined,
+    Menu,
     MenuUnfoldOutlined,
     MenuFoldOutlined,
   },
@@ -79,12 +56,6 @@ export default defineComponent({
 
 #components-layout-demo-custom-trigger .trigger:hover {
   color: #1890ff;
-}
-
-#components-layout-demo-custom-trigger .logo {
-  height: 32px;
-  background: rgba(255, 255, 255, 0.3);
-  margin: 16px;
 }
 
 .site-layout .site-layout-background {
--- frontend/src/components/core/Menu.vue
+++ frontend/src/components/core/Menu.vue
@@ -1,11 +1,74 @@
 <template>
-  <h1>Menu</h1>
+  <a-layout-sider :collapsed="collapsed" :trigger="null" collapsible>
+    <div class="logo" @click="toggleCollapsed" />
+    <a-menu v-model:selectedKeys="selectedKeys" theme="dark" mode="inline">
+      <a-menu-item key="1">
+        <user-outlined />
+        <span>nav 1</span>
+      </a-menu-item>
+      <a-menu-item key="2">
+        <video-camera-outlined />
+        <span>nav 2</span>
+      </a-menu-item>
+      <a-menu-item key="3">
+        <upload-outlined />
+        <span>nav 3</span>
+      </a-menu-item>
+    </a-menu>
+  </a-layout-sider>
 </template>
 
 <script lang="ts">
+import { ref } from "vue";
+import {
+  UserOutlined,
+  VideoCameraOutlined,
+  UploadOutlined,
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
+} from "@ant-design/icons-vue";
 export default {
   name: "Menu",
+  props: ["collapsed"],
+  emits: ["update:collapsed"],
+  components: {
+    UserOutlined,
+    VideoCameraOutlined,
+    UploadOutlined,
+    MenuUnfoldOutlined,
+    MenuFoldOutlined,
+  },
+  setup(props, { emit }) {
+    const selectedKeys = ref<string[]>(["2"]);
+    const toggleCollapsed = () => {
+      emit("update:collapsed", !props.collapsed);
+    };
+
+    return { selectedKeys, toggleCollapsed };
+  },
 };
 </script>
 
-<style></style>
+<style>
+#components-layout-demo-custom-trigger .trigger {
+  font-size: 18px;
+  line-height: 64px;
+  padding: 0 24px;
+  cursor: pointer;
+  transition: color 0.3s;
+}
+
+#components-layout-demo-custom-trigger .trigger:hover {
+  color: #1890ff;
+}
+
+.logo {
+  height: 32px;
+  background: rgba(255, 255, 255, 0.3);
+  margin: 16px;
+}
+
+.site-layout .site-layout-background {
+  background: #fff;
+}
+</style>
