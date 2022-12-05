99a589fbf98fc6a3b1548421e20c25a348378c12
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-12-05 20:10:29 +0700

    ep16_revise_header_ui

--- frontend/src/components/core/Header.vue
+++ frontend/src/components/core/Header.vue
@@ -1,16 +1,27 @@
 <template>
-  <a-layout-header style="background: #fff; padding: 0">
-    <menu-unfold-outlined
-      v-if="collapsed"
-      class="trigger"
-      @click="toggleCollapsed"
-    />
-    <menu-fold-outlined v-else class="trigger" @click="toggleCollapsed" />
+  <a-layout-header class="bg-green-700 pl-7 pr-0 text-white">
+    <div style="display: flex; flex-direction: row; align-items: center">
+      <menu-unfold-outlined
+        v-if="collapsed"
+        class="trigger"
+        @click="toggleCollapsed"
+      />
+      <menu-fold-outlined v-else class="trigger" @click="toggleCollapsed" />
+      <span style="flex-grow: 1; margin-left: 10px">CMStock</span>
+      <a-button type="text">
+        <LogoutOutlined style="color: white" />
+        <span style="color: white">Logout</span>
+      </a-button>
+    </div>
   </a-layout-header>
 </template>
 
 <script lang="ts">
-import { MenuUnfoldOutlined, MenuFoldOutlined } from "@ant-design/icons-vue";
+import {
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
+  LogoutOutlined,
+} from "@ant-design/icons-vue";
 
 export default {
   name: "Header",
@@ -19,6 +30,7 @@ export default {
   components: {
     MenuUnfoldOutlined,
     MenuFoldOutlined,
+    LogoutOutlined,
   },
   setup(props, { emit }) {
     const toggleCollapsed = () => {
