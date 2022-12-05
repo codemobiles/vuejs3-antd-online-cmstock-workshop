11cf08b65f593b871703c12b0b02448d1537638f
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-12-05 20:18:45 +0700

    ep17_header_size

--- frontend/src/components/core/Header.vue
+++ frontend/src/components/core/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <a-layout-header class="bg-green-700 pl-7 pr-0 text-white">
+  <a-layout-header class="bg-green-700 pl-7 pr-5 text-white">
     <div style="display: flex; flex-direction: row; align-items: center">
       <menu-unfold-outlined
         v-if="collapsed"
@@ -7,10 +7,12 @@
         @click="toggleCollapsed"
       />
       <menu-fold-outlined v-else class="trigger" @click="toggleCollapsed" />
-      <span style="flex-grow: 1; margin-left: 10px">CMStock</span>
-      <a-button type="text">
-        <LogoutOutlined style="color: white" />
-        <span style="color: white">Logout</span>
+      <span style="flex-grow: 1; margin-left: 10px; font-size: 18px"
+        >CMStock</span
+      >
+      <a-button type="text" class="p-0">
+        <LogoutOutlined style="color: white" class="trigger" />
+        <span style="color: white; font-size: 18px">Logout</span>
       </a-button>
     </div>
   </a-layout-header>
@@ -42,4 +44,11 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.trigger {
+  font-size: 18px;
+  line-height: 64px;
+  cursor: pointer;
+  transition: color 0.3s;
+}
+</style>
