3e525298d70670e6cbf975f10a30e3d4f35b1915
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-12-17 16:42:27 +0700

    ep19_card_and_form

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,9 +1,65 @@
 <template>
-  <h1>Login</h1>
+  <div>
+    <a-card
+      hoverable
+      style="
+        box-shadow: rgba(0, 0, 0, 0.15) 0px 2px 5px 0px;
+        width: 300px;
+        height: 500px;
+      "
+    >
+      <template #cover>
+        <img alt="example" src="@/assets/03-vuejs-login.png" />
+      </template>
+
+      <h1 className="text-2xl font-bold pb-3">Login</h1>
+
+      <a-form
+        name="basic"
+        :label-col="{ span: 8 }"
+        :wrapper-col="{ span: 16 }"
+        autocomplete="off"
+      >
+        <a-form-item
+          label="Username"
+          name="username"
+          :rules="[{ required: true, message: 'Please input your username!' }]"
+        >
+          <a-input />
+        </a-form-item>
+
+        <a-form-item
+          label="Password"
+          name="password"
+          :rules="[{ required: true, message: 'Please input your password!' }]"
+        >
+          <a-input-password />
+        </a-form-item>
+
+        <a-form-item name="remember" :wrapper-col="{ offset: 8, span: 16 }">
+          <a-checkbox>Remember me</a-checkbox>
+        </a-form-item>
+
+        <a-form-item :wrapper-col="{ offset: 8, span: 16 }">
+          <a-button type="primary" html-type="submit">Submit</a-button>
+        </a-form-item>
+      </a-form>
+    </a-card>
+  </div>
 </template>
 
 <script lang="ts">
+import { UserOutlined, LockOutlined } from "@ant-design/icons-vue";
+import { defineComponent, reactive, computed } from "vue";
+import type { UnwrapRef } from "vue";
+import type { FormProps } from "ant-design-vue";
+import type { User } from "@/models/user.model";
+
 export default {
+  components: {
+    UserOutlined,
+    LockOutlined,
+  },
   name: "Login",
 };
 </script>
