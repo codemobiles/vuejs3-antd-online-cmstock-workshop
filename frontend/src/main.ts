2734c85a23e46f78a0296c606002685ef7453858
LEK CodeMobiles <chaiyasit.t@gmail.com>
2023-06-08 17:56:53 +0700

    📦 ADD dayjs, vue-chart-3

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -12,13 +12,18 @@ import Antd from 'ant-design-vue'
 
 import axios from 'axios'
 
+import dayjs from 'dayjs'
+
 const axiosInstance = axios.create({
   withCredentials: true
 })
 
 const app = createApp(App)
+
 app.use(createPinia())
 app.use(Antd)
 app.use(router)
+app.provide('dayjs', dayjs)
 app.config.globalProperties.$axios = { ...axiosInstance }
+app.config.globalProperties.$dayjs = dayjs
 app.mount('#app')
