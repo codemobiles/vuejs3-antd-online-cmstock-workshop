810e350fce85c673657c5e79ab1a4859efe82483
LEK CodeMobiles <chaiyasit.t@gmail.com>
2023-06-08 17:57:24 +0700

    ✨ NEW filters service

---
+++ frontend/src/services/filters.ts
@@ -0,0 +1,21 @@
+import { imageUrl } from "@/services/constants";
+import dayjs from "dayjs";
+
+export default {
+  currency(value: any) {
+    return new Intl.NumberFormat("th-TH", {
+      style: "currency",
+      currency: "THB",
+    }).format(value);
+  },
+  thousand(value: any) {
+    return new Intl.NumberFormat("en-IN").format(value);
+  },
+  formatDate(dateString: string) {
+    const date = dayjs(dateString);
+    return date.format("dddd MMMM D, YYYY hh:mm");
+  },
+  fullImageUrl(image: any) {
+    return `${imageUrl}/${image}?timestamp=${Date.now()}`;
+  },
+};
