8413364da1367fcf8f98870e3477fa9aeeaedfac
LEK CodeMobiles <chaiyasit.t@gmail.com>
2023-06-08 17:58:25 +0700

    ✅ Report with chart

--- frontend/src/components/charts/BarChart.vue
+++ frontend/src/components/charts/BarChart.vue
@@ -1,36 +1,53 @@
 <template>
-  <Bar id="my-chart-id" :options="chartOptions" :data="chartData" />
+  <a-row>
+    <a-col>
+      <a-card class="tw-rounded-md tw-drop-shadow-sm" type="inner">
+        <template #title>
+          <span class="tw-font-bold tw-text-[0.8rem]"> Profit this day </span>
+          <span class="tw-font-bold tw-text-[1.5rem] tw-block"> {{ filters.currency(1999) }} </span>
+        </template>
+        <BarChart v-bind="barChartProps"  :options="options" class="tw-h-full md:tw-max-h-[200px]" />
+      </a-card>
+    </a-col>
+  </a-row>
 </template>
-<script lang="ts">
-import { Bar } from 'vue-chartjs'
-import {
-  Chart as ChartJS,
-  Title,
-  Tooltip,
-  Legend,
-  BarElement,
-  CategoryScale,
-  LinearScale
-} from 'chart.js'
-import { defineComponent, reactive } from 'vue'
 
-ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+<script lang="ts" setup>
+import { Chart, registerables } from 'chart.js'
+import { BarChart, useBarChart } from 'vue-chart-3'
+import { ref, computed, defineComponent, defineProps } from 'vue'
+import filters from '@/services/filters'
 
-export default defineComponent({
-  name: 'BarChart',
-  components: { Bar },
-  setup() {
-    const chartData = reactive({
-      labels: ['January', 'February', 'March'],
-      datasets: [{ data: [40, 20, 12] }]
-    })
-    const chartOptions = reactive({
-      responsive: true
-    })
-    return {
-      chartData,
-      chartOptions
+Chart.register(...registerables)
+
+const props = defineProps({
+  data: Array
+})
+const options = ref({
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top',
+      display: false
+    },
+    title: {
+      display: false,
+      text: 'Monthly'
     }
   }
+})
+
+const chartData = computed(() => ({
+  labels: ['SUN', 'MON', 'TUE', 'WED', 'THU','FRI','SAT'],
+  datasets: [
+    {
+      data: props.data as any,
+      backgroundColor: ['#EF476F', '#FFD166', '#7A306C', '#8E8DBE', '#A9E4EF', '#81F495', '#96F550']
+    }
+  ]
+}))
+
+const { barChartProps, barChartRef } = useBarChart({
+  chartData
 })
 </script>
--- frontend/src/views/Report.vue
+++ frontend/src/views/Report.vue
@@ -1,14 +1,56 @@
 <template>
-  <a-row>
-    <a-col :span="24">
-      <BarChart />
+  <a-row :gutter="[20, 20]">
+    <a-col :span="24" :md="16">
+      <a-row :gutter="[20, 20]">
+        <a-col :span="24">
+          <LineChart :data="lineData" class="tw-h-full" />
+        </a-col>
+        <a-col :span="24">
+          <a-button type="primary" @click="shuffleData"> Shuffle </a-button>
+        </a-col>
+      </a-row>
+    </a-col>
+    <a-col :span="24" :md="8" class="tw-h-full">
+      <a-row align="space-between" justify="center" class="tw-h-full" :gutter="[20, 20]">
+        <a-col :span="24">
+          <PieChart :data="pieData" class=" tw-h-full md:tw-max-h-[45vh] tw-overflow-auto"
+        /></a-col>
+        <a-col :span="24">
+          <BarChart :data="barData" class="tw-h-full md:tw-max-h-[45vh] tw-overflow-auto"
+        /></a-col>
+      </a-row>
     </a-col>
   </a-row>
 </template>
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
+import PieChart from '@/components/charts/PieChart.vue'
+import LineChart from '@/components/charts/LineChart.vue'
 import BarChart from '@/components/charts/BarChart.vue'
 export default defineComponent({
-  components: { BarChart }
+  components: { BarChart, LineChart, PieChart },
+  setup() {
+    const lineData = ref([30, 40, 60, 70, 5, 35, 12, 75, 42, 33, 10, 2])
+    const barData = ref([30, 40, 60, 70, 5, 10, 10])
+    const pieData = ref([30, 40, 60])
+    const shuffleData = () => {
+      let array = ref([lineData, barData, pieData])
+      for (let [index, dataset] of array.value.entries()) {
+        let tempDataset = dataset.value
+
+        for (let i = tempDataset.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1))
+          ;[tempDataset[i], tempDataset[j]] = [tempDataset[j], tempDataset[i]]
+        }
+        array.value[index].value = tempDataset
+      }
+    }
+    return {
+      shuffleData,
+      lineData,
+      barData,
+      pieData
+    }
+  }
 })
 </script>
