3f86095e9cd7d9d6b577591f4481f8bcfd0aae57
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-06-14 18:30:59 +0700

    ep48_stock_pinia_store_4

--- frontend/src/stores/useStockStore.ts
+++ frontend/src/stores/useStockStore.ts
@@ -1,7 +1,9 @@
 import { FetchingStatus } from "@/models/fetchingStatus.enum";
 import { Product } from "@/models/product.model";
+import { server } from "@/services/constants";
 import { defineStore } from "pinia";
 import { reactive, ref } from "vue";
+import axios from "axios";
 
 const dummyData: Product[] = [
   {
@@ -25,6 +27,10 @@ export const useStockStore = defineStore("stock", () => {
   const autocompleteOptions = ref([]);
   const stocks = ref<Product[]>(dummyData);
   const fetchingStatus = ref<FetchingStatus>(FetchingStatus.init);
+
+  const getProducts = () => {
+    return axios.get("http://localhost:8081/api/v2/" + server.PRODUCT_URL);
+  };
 
   return {
     // loadProducts,
--- frontend/src/views/Stock.vue
+++ frontend/src/views/Stock.vue
@@ -5,6 +5,39 @@
 import { useStockStore } from "@/stores/useStockStore";
 export default {
   setup() {
+
+    const columns = [
+      {
+        title: 'Image',
+        dataIndex: 'image'
+      },
+      {
+        name: 'Name',
+        dataIndex: 'name',
+        width: '30%'
+      },
+      {
+        title: 'Price',
+        dataIndex: 'price'
+      },
+      {
+        title: 'Stock',
+        dataIndex: 'stock'
+      },
+      {
+        title: 'Created At',
+        dataIndex: 'createdAt'
+      },
+      {
+        title: 'Updated At',
+        dataIndex: 'updatedAt'
+      },
+      {
+        title: 'Action',
+        dataIndex: 'action'
+      }
+    ]
+    
     const stockStore = useStockStore();
     const dataSource = stockStore.stocks;
 
