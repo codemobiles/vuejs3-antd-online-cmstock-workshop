89164a684573e118299141c515c0d76f8c86ab76
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-06-15 12:18:12 +0700

    ep56_stock_pinia_store_13

--- frontend/src/views/Stock.vue
+++ frontend/src/views/Stock.vue
@@ -1,66 +1,15 @@
 <template>
-  <div>
-    <a-table
-      :columns="columns"
-      :data-source="stockStore.stocks"
-      :pagination="{ position: ['topRight'] }"
+  <a-row justify="space-around" align="center">
+    <a-col
+      :span="12"
+      :lg="6"
+      v-for="(item, i) in stockCardList"
+      :key="i"
+      class="tw-mb-2 tw-px-2"
     >
-      <template #headerCell="{ column }">
-        <span class="tw-font-bold"> {{ column.title }} </span>
-      </template>
-
-      <template #bodyCell="{ column, record }">
-        <template v-if="column.dataIndex === 'name'">
-          <div class="tw-block tw-truncate tw-overflow-ellipsis">
-            <a @click="routeToEdit(record.id)">
-              {{ record.name }}
-            </a>
-          </div>
-        </template>
-        <template v-else-if="column.dataIndex === 'image'">
-          <img
-            :src="stockStore.getProductImage(record.image)"
-            lazy-src="https://picsum.photos/id/11/10/6"
-            aspect-ratio="1"
-            class="tw-object-contain tw-cursor-pointer tw-transition-all hover:tw-scale-[1.1]"
-            width="50"
-            height="50"
-          />
-        </template>
-        <template v-else-if="column.dataIndex === 'price'">
-          <span>{{ filters.currency(record.price) }}</span>
-        </template>
-        <template v-else-if="column.dataIndex === 'stock'">
-          <a-tag>{{ record.stock }} pcs</a-tag>
-        </template>
-        <template v-else-if="column.dataIndex === 'createdAt'">
-          <span class="tw-text-gray-600">{{
-            filters.formatDate(record.createdAt)
-          }}</span>
-        </template>
-        <template v-else-if="column.dataIndex === 'updatedAt'">
-          <span class="tw-text-gray-600">{{ record.updatedAt }}</span>
-        </template>
-        <template v-else-if="column.dataIndex === 'action'">
-          <a-row align="center">
-            <a-button
-              class="tw-bg-[#ffd155ff] tw-border-[#ffd155ff] tw-text-white"
-              @click="$router.push(`/stock-edit/${record.id}`)"
-            >
-              <EditFilled class="tw-pb-2" />
-            </a-button>
-
-            <a-popconfirm title="Are you sure？">
-              <template #icon><QuestionCircleOutlined /></template>
-              <a-button type="danger">
-                <DeleteFilled class="tw-pb-2"
-              /></a-button>
-            </a-popconfirm>
-          </a-row>
-        </template>
-      </template>
-    </a-table>
-  </div>
+      <span>xxx</span>
+    </a-col>
+  </a-row>
 </template>
 <script lang="ts">
 import { useStockStore } from "@/stores/useStockStore";
@@ -69,6 +18,33 @@ import { useRouter } from "vue-router";
 
 export default {
   setup() {
+    const stockCardList = [
+      {
+        title: "Total",
+        amount: 1800,
+        icon: "ShoppingCartOutlined",
+        color: "#039C52",
+      },
+      {
+        title: "Sold-out",
+        amount: 12,
+        icon: "ExperimentOutlined",
+        color: "#F18F15",
+      },
+      {
+        title: "Return",
+        amount: 2,
+        icon: "RollbackOutlined",
+        color: "#D94231",
+      },
+      {
+        title: "Discount",
+        amount: 101,
+        icon: "GiftOutlined",
+        color: "#03B8EA",
+      },
+    ];
+
     const columns = [
       {
         title: "Id",
@@ -115,6 +91,7 @@ export default {
     };
 
     return {
+      stockCardList,
       routeToEdit,
       stockStore,
       columns,
