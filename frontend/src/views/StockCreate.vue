517ed462493fa7108ccd53fed9968c70f93cc55b
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-06-22 08:56:04 +0700

    vue3_cmstock_ep71_stock_28

--- frontend/src/views/StockCreate.vue
+++ frontend/src/views/StockCreate.vue
@@ -1,13 +1,118 @@
 <template>
-  <div>Stock Create</div>
+  <a-row align="center" justify="center" class="tw-w-full">
+    <a-card
+      class="ant-card-body"
+      style="box-shadow: rgba(0, 0, 0, 0.15) 0px 2px 5px 0px; width: 700px"
+    >
+      <template #title>
+        <a-tag color="success" class="tw-px-4 tw-py-2">
+          <a-row align="center" class="tw-h-full tw-items-center">
+            <PlusCircleFilled class="tw-text-[1rem] tw-pr-2" />
+            <span class="tw-font-bold tw-text-[1.5rem]">Create Product</span>
+          </a-row>
+        </a-tag>
+      </template>
+
+      <!-- card body -->
+      <a-form @finish="onSubmit">
+        <a-form-item labelAlign="right" label="Name" name="name" has-feedback>
+          <a-input v-model:value="formState.name" />
+        </a-form-item>
+
+        <a-form-item :wrapper-col="{ span: 24 }">
+          <a-row align="center" justify="end" :gutter="[10, 10]">
+            <a-button type="primary" html-type="submit">Create</a-button>
+            <a-button style="margin-left: 10px" @click="$router.push('/stock')"
+              >Cancel</a-button
+            >
+          </a-row>
+        </a-form-item>
+      </a-form>
+
+      <span @click="formState.name = ''">#Debug: {{ formState.name }}</span>
+    </a-card>
+  </a-row>
 </template>
-
 <script lang="ts">
-export default {
-  setup() {
-    return {};
-  },
-};
-</script>
+import { defineComponent, reactive, ref, toRaw } from "vue";
+import { Form } from "ant-design-vue";
+import api from "@/services/api";
+import router from "@/router";
+import type { Rule } from "ant-design-vue/lib/form";
+import { PlusCircleFilled } from "@ant-design/icons-vue";
 
-<style lang="scss" scoped></style>
+export default defineComponent({
+  components: {
+    PlusCircleFilled,
+  },
+  setup() {
+    const previewVisible = ref(false);
+    const previewTitle = ref("");
+    const formState = reactive({
+      name: "",
+      price: 2000,
+      stock: 20,
+      image: null,
+      imageURL: null,
+    });
+
+    const onSubmit = async () => {
+      alert("Submit");
+    };
+
+    const onFileSelected = (event: any) => {
+      // for preview
+      const reader = new FileReader();
+      reader.onload = (event: any) => {
+        formState.imageURL = event.target.result;
+      };
+
+      // for upload
+      reader.readAsDataURL(event.target.files[0]);
+      formState.image = event.target.files[0];
+    };
+
+    const handleCancel = () => {
+      previewVisible.value = false;
+      previewTitle.value = "";
+    };
+
+    const validateText = (rule: Rule, value: string) => {
+      if (value === "") {
+        return Promise.reject("Please input the name");
+      }
+      return Promise.resolve();
+    };
+
+    const validatePrice = (rule: Rule, value: string) => {
+      if (Number(value) < 100) {
+        return Promise.reject("price must be a least at 100");
+      }
+      return Promise.resolve();
+    };
+
+    const validateStock = (rule: Rule, value: string) => {
+      if (Number(value) < 10) {
+        return Promise.reject("stock must be a least at 10");
+      }
+      return Promise.resolve();
+    };
+
+    const rules = {
+      name: [{ required: true, validator: validateText, trigger: "change" }],
+      price: [{ validator: validatePrice, trigger: "change" }],
+      stock: [{ validator: validateStock, trigger: "change" }],
+    };
+
+    return {
+      rules,
+      formState,
+      onSubmit,
+      onFileSelected,
+      handleCancel,
+      previewTitle,
+      previewVisible,
+    };
+  },
+});
+</script>
